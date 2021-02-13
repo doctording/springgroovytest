b0cfe40fe678bb9c66b18730e20e17c865b9b3ab
doctording <dingwenlong2014@163.com>
2021-02-13 11:51:23 +0800

    fix readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 spring + groovy
 
-直接运行api，然后脚本或浏览器访问api即可
+直接运行api，然后脚本或浏览器访问api即可(相当于开了后面，可以对任意bean进行方法调用执行)
+
+![](./imgs/test.png)
--- test.groovy
+++ test.groovy
@@ -1,16 +1,25 @@
 import org.springframework.beans.factory.annotation.Autowired
-import com.example.service.TbUserService;
+import com.example.service.TbUserService
 
 class Foo {
 
     @Autowired
-    TbUserService tbUserService;
+    TbUserService tbUserService
+
+//    Object run() {
+//        // do something
+//        def f = tbUserService.getById(1)
+//        if (f != null) {
+//            return f.name
+//        }
+//        return null
+//    }
 
     Object run() {
         // do something
-        def f = tbUserService.getById(1);
+        def f = tbUserService.fortest()
         if (f != null) {
-            return f.name
+            return f
         }
         return null
     }
